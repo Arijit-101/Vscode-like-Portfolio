afcacfd2a0a4d857f76ff2e9fd655efb93263501
Arijit-101 <arijitsaha3000@gmail.com>
2022-06-06 11:22:02 +0530

    third commit

--- src/screens/main.js
+++ src/screens/main.js
@@ -18,8 +18,9 @@ function Main() {
 			<div className='flex flex-row  bg-red-400'>
 				<Sidebar />
 				<Routes>
-					<Route path='/' element={<Home />} />
-					<Route path='/Vscode-like-Portfolio' element={<Home />} />
+					<Route path={'/' | '/Vscode-like-Portfolio'} element={<Home />} />
+				
+					<Route path='arijit-101.github.io/Vscode-like-Portfolio/' element={<Home />} />
 					<Route path='/about' element={<About />} />
 					<Route path='/contact' element={<Contact />} />
 					<Route path='/projects' element={<Projects />} />
